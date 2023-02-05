4de35d29e80cfb0001b78f5d84d18c9ba83ac86d
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2023-02-05 19:54:58 +0100

    ApiService Abstract cleaned.

--- src/app/services/weather-api/abstract-weather-api-service.ts
+++ src/app/services/weather-api/abstract-weather-api-service.ts
@@ -1,8 +1,7 @@
 import { LocationDataModel } from '../../components/location-picker/location-data.model';
 import { Observable } from 'rxjs';
-import {WeatherApiData, WeatherApiResponse} from './weather-api-response';
+import { WeatherApiData } from './weather-api-response';
 
 export abstract class AbstractWeatherApiService {
-  // public abstract getWeatherData(locationData: LocationDataModel): Observable<WeatherApiResponse>;
   public abstract getWeatherData(locationData: LocationDataModel): Observable<WeatherApiData | []>;
 }
