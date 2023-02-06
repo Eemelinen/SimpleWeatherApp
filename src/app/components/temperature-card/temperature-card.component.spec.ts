43840181ac2a67b4501e753ac554b7b965d8297b
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2023-02-06 23:39:58 +0100

    Tests added to temperature card.

--- src/app/components/temperature-card/temperature-card.component.spec.ts
+++ src/app/components/temperature-card/temperature-card.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TemperatureCardComponent } from './temperature-card.component';
 
+const title = 'Monday';
+const temp = 12;
+
 describe('TemperatureCardComponent', () => {
   let component: TemperatureCardComponent;
   let fixture: ComponentFixture<TemperatureCardComponent>;
@@ -14,10 +17,36 @@ describe('TemperatureCardComponent', () => {
 
     fixture = TestBed.createComponent(TemperatureCardComponent);
     component = fixture.componentInstance;
+    component.title = title;
+    component.temperature = temp;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the title received as input', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.info').textContent).toContain(title);
+  });
+
+  it('should render the temperature received as input', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.temperature').textContent).toContain(temp);
+  });
+
+  it('if input bigCard is true should add the class big-card', () => {
+    const compiled = fixture.nativeElement;
+    component.bigCard = true;
+    fixture.detectChanges();
+    expect(compiled.querySelector('.card').classList).toContain('big-card');
+  });
+
+  it('if input bigCard is false should not add the class big-card', () => {
+    const compiled = fixture.nativeElement;
+    component.bigCard = false;
+    fixture.detectChanges();
+    expect(compiled.querySelector('.card').classList).not.toContain('big-card');
   });
 });
--- src/app/services/location/location.service.spec.ts
+++ src/app/services/location/location.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LocationService } from './location.service';
+import { availableCountries } from './countries';
 
 describe('LocationService', () => {
   let service: LocationService;
@@ -12,5 +13,11 @@ describe('LocationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return countries', function () {
+    const countries = service.getAvailableCountries();
+    expect(countries.length).toBeGreaterThan(0);
+    expect(countries[0]).toBe(availableCountries[0]);
   });
 });
