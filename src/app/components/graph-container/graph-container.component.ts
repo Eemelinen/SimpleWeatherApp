8bb0ed89b9b0880d24abed09d4b76c3e7f04d367
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2023-02-09 12:51:02 +0100

    Line chart works and looks decent

--- src/app/components/graph-container/graph-container.component.ts
+++ src/app/components/graph-container/graph-container.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-graph-container',
   templateUrl: './graph-container.component.html',
   styleUrls: ['./graph-container.component.scss']
 })
-export class GraphContainerComponent {
+export class GraphContainerComponent implements OnInit {
+  public chart!: Chart;
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart(){
+
+    this.chart = new Chart("lineChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {
+        labels: ['', '', '', '', '', '', ''],
+        datasets: [
+          {
+            label: "Temperature",
+            data: ['1','6', '7', '8', '11', '5', '3', '4'],
+          },
+        ]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        // aspectRatio: 2.5,
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            display: false,
+          },
+          y: {
+            display: false,
+          }
+        }
+      }
+
+    });
+  }
 
 }
