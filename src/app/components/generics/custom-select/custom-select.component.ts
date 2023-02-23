04a3a0eadfe2d6efe8a96bdccfb414d44989c1f1
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2023-02-23 16:07:28 +0100

    Relic code removed from custom select.

--- src/app/components/generics/custom-select/custom-select.component.ts
+++ src/app/components/generics/custom-select/custom-select.component.ts
@@ -20,8 +20,6 @@ export class CustomSelectComponent {
   public dropdownOpen: boolean = false;
   public get dropdownElement(): Element {return this.elem.nativeElement.querySelector('.dropdown-list')}
 
-  private currentIndex = -1;
-
   constructor(
     private elem: ElementRef
   ) { }
@@ -30,42 +28,9 @@ export class CustomSelectComponent {
     this.currentValue = this.options[0];
   }
 
-  handleKeyboardEvents($event: KeyboardEvent) {
-    if (this.dropdownOpen) {
-      $event.preventDefault();
-    } else {
-      return;
-    }
-    if ($event.code === 'ArrowUp') {
-      if (this.currentIndex < 0) {
-        this.currentIndex = 0;
-      } else if (this.currentIndex > 0) {
-        this.currentIndex--;
-      }
-      this.elem.nativeElement.querySelectorAll('li').item(this.currentIndex).focus();
-    } else if ($event.code === 'ArrowDown') {
-      if (this.currentIndex < 0) {
-        this.currentIndex = 0;
-      } else if (this.currentIndex < this.options.length-1) {
-        this.currentIndex++;
-      }
-      this.elem.nativeElement.querySelectorAll('li').item(this.currentIndex).focus();
-    } else if (($event.code === 'Enter' || $event.code === 'NumpadEnter') && this.currentIndex >= 0) {
-      this.selectByIndex(this.currentIndex);
-    } else if ($event.code === 'Escape') {
-      this.closeDropdown();
-    }
-  }
-
   closeDropdown() {
     this.dropdownElement.setAttribute('aria-expanded', "false");
-    this.currentIndex = -1;
     this.dropdownOpen = false;
-  }
-
-  selectByIndex(i: number) {
-    let value = this.options[i];
-    this.select(value);
   }
 
   select(value: any) {
